304b4e00746fa1d7c9f429a13b492c28f64ec2ea
srinivas prayag sahu <sprayagsahu@gmail.com>
2021-02-13 21:39:11 +0530

    Initial Commit

---
+++ BigCatTests/BigCatTests.swift
@@ -0,0 +1,33 @@
+//
+//  BigCatTests.swift
+//  BigCatTests
+//
+//  Created by Sidhartha Das on 13/02/21.
+//
+
+import XCTest
+@testable import BigCat
+
+class BigCatTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
